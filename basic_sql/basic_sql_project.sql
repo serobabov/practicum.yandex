e699fc39d3b4ad169f6629cf5b7234e5c4bff31c
Сергей Серобабов <me@sserobabov.ru>
2024-08-20 18:37:02 +0300

    Update basic_sql_project.sql

--- basic_sql/basic_sql_project.sql
+++ basic_sql/basic_sql_project.sql
@@ -1,1 +1,12 @@
+/* Задача 1/23: Отобразите все записи из таблицы `company` по компаниям, которые закрылись. */
+SELECT *
+FROM company
+WHERE status = 'closed';
+
+/* Задача 2/23: Отобразите количество привлечённых средств для новостных компаний США. Используйте данные из таблицы `company`. Отсортируйте таблицу по убыванию значений в поле `funding_total`. */
+SELECT funding_total
+FROM company
+WHERE category_code = 'news'
+  AND country_code = 'USA'
+ORDER BY funding_total DESC;
 
